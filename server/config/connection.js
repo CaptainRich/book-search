094b72c12f4a2132d332255f9a30a3f556498f96
RichA <rich_ay@sbcglobal.net>
2021-01-07 21:43:06 -0600

    tweaked onnection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/book-search', {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/book-search", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: true,
-});    
-
+  useFindAndModify: false,
+}).catch ((err) => {
+  console.error(err);
+})
 // To debug Mongoose activity, uncomment the following debug line.
 // mongoose.set( 'debug', true );
 
